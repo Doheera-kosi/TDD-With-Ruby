020cfebcd20d0b649cacc3d853062857f90d8e20
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-01-14 15:02:42 +0500

    Adding code to pass additional tests

--- solver.rb
+++ solver.rb
@@ -1,15 +1,17 @@
 class Solver
-    def factorial(num, product = 1)
-      return product if [1, 0].include?(num)
-  
-      factorial(num - 1, product * num)
+  def factorial(num, product = 1)
+    raise ArgumentError, 'Negative numbers are not allowed' if num.negative?
+    return product if [1, 0].include?(num)
+    factorial(num - 1, product * num)
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     end
-  
-  
-    def fizzbuzz(num)
-      if (num % 3).zero?
-        'fizz'
-      end
-    end
+  end
 end
-  

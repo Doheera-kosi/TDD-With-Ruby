31190d24f8dd2b9ebdee161c0401478b537108f8
Doheera-kosi <kupourevans99@gmail.com>
2023-01-14 10:12:06 +0000

    Modify solver.rb

--- solver.rb
+++ solver.rb
@@ -2,7 +2,12 @@ class Solver
   def factorial(num, product = 1)
     raise ArgumentError, 'Negative numbers are not allowed' if num.negative?
     return product if [1, 0].include?(num)
+
     factorial(num - 1, product * num)
+  end
+
+  def reverse_string(str)
+    str.chars.reverse.join
   end
 
   def fizzbuzz(num)
@@ -12,6 +17,8 @@ class Solver
       'fizz'
     elsif (num % 5).zero?
       'buzz'
+    else
+      num.to_s
     end
   end
 end
